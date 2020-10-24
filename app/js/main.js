7a302b8aa64a332a22de377e95b934c4cfb31217
xolesteroll <xol2dem@gmail.com>
2020-10-25 01:52:18 +0300

    modal, and svg icons

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         slidesWrapper = document.querySelector('.banner__slider-wrapper'),
         slidesField = document.querySelector('.banner__slider-inner'),
         width = window.getComputedStyle(slidesWrapper).width;
+
     let offset = 0;
 
     slidesField.style.width = 100 * slides.length + '%';
@@ -47,33 +48,33 @@ document.addEventListener("DOMContentLoaded", function () {
         reviewPrev = document.querySelector('.reviews__slider-prev'),
         reviewSliderWrapper = document.querySelector('.reviews__slider-wrapper'),
         reviewSliderInner = document.querySelector('.reviews__slider-inner'),
+        marginLeft = +window.getComputedStyle(reviewSlides[1]).marginLeft.slice(0, (window.getComputedStyle(reviewSlides[1]).marginLeft.length - 2)),
         wrapperWidth = window.getComputedStyle(reviewSliderWrapper).width;
     let reviewOffset = 0;
-    console.log(wrapperWidth);
 
     reviewSliderInner.style.width = 100 * reviewSlides.length + '%';
 
     reviewSlides.forEach(slide => {
-        slide.style.width = +wrapperWidth.slice(0, wrapperWidth.length - 2) / 2 - 23 + 'px';
+        slide.style.width = +wrapperWidth.slice(0, wrapperWidth.length - 2) / 2 - (marginLeft / 2) + 'px';
     });
 
     reviewNext.addEventListener('click', () => {
         if (reviewOffset >= (+wrapperWidth.slice(0, wrapperWidth.length - 2) * ((Math.round(reviewSlides.length) / 2) - 1))){
             reviewOffset = 0;
         } else {
-            reviewOffset += +wrapperWidth.slice(0, wrapperWidth.length - 2) + 46;
+            reviewOffset += +wrapperWidth.slice(0, wrapperWidth.length - 2) + marginLeft;
         }
 
         reviewSliderInner.style.transform = `translateX(-${reviewOffset}px)`;
 
-        console.log(+wrapperWidth.slice(0, wrapperWidth.length - 2) + 46);
+        console.log(+wrapperWidth.slice(0, wrapperWidth.length - 2) + marginLeft);
     });
 
     reviewPrev.addEventListener('click', () => {
         if (reviewOffset ==  0){
-            reviewOffset = (+wrapperWidth.slice(0, wrapperWidth.length - 2) + 46) * ((Math.round(reviewSlides.length) / 2) - 1);
+            reviewOffset = (+wrapperWidth.slice(0, wrapperWidth.length - 2) + marginLeft) * ((Math.round(reviewSlides.length) / 2) - 1);
         } else {
-            reviewOffset -= +wrapperWidth.slice(0, wrapperWidth.length - 2) + 46;
+            reviewOffset -= +wrapperWidth.slice(0, wrapperWidth.length - 2) + marginLeft;
         }
 
         reviewSliderInner.style.transform = `translateX(-${reviewOffset}px)`;
@@ -81,6 +82,30 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(+wrapperWidth.slice(0, wrapperWidth.length - 2) * ((Math.round(reviewSlides.length) / 2) - 1));
     });
 
+    //Modal
 
+    const modal = document.querySelector('.modal'),
+        closeBtns = document.querySelectorAll('.modal__close'),
+        openBtns = document.querySelectorAll('.about__subscribe');
+
+    function openModal(window){
+        openBtns.forEach(btn => {
+            btn.addEventListener('click', () =>{
+                window.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            });
+        });
+    }
+    function closeModal(window){
+        closeBtns.forEach(btn => {
+            btn.addEventListener('click', () =>{
+                window.style.display = 'none';
+                document.body.style.overflow = '';
+            });
+        });
+
+    }
+    openModal(modal);
+    closeModal(modal);
 
 });
